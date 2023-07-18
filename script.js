b7375aa9ae50b68b1de0a262ec29a2e0d42eea02
DenisGamzikov <132223000+DenisGamzikov@users.noreply.github.com>
2023-07-18 13:47:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+function filter(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const filterPopulation = formData.get("filterPopulation");
+  const citySize = formData.get("citySize");
+}
 const loaderEl = document.getElementById("loader");
+const spanEl = document.getElementById("counter");
 function loadingData() {
   loaderEl.classList.add("d-block");
   fetch("./cities+.json")
@@ -7,16 +14,19 @@ function loadingData() {
       console.log({ cities });
       const tableEl = document.getElementById("table-data");
       let citiesHtml = "";
+
       cities.forEach((city) => {
         citiesHtml += `
     <tr>
     <td> ${city.address}</td>
-    <td class="population"> ${city.population}</td>
-    <td class="age"> ${city.foundation_year}</td>
+    <td class="population">${city.population}</td>
+    <td class="age">${2023 - city.foundation_year}</td>
     </tr>`;
       });
       loaderEl.classList.add("d-none");
       tableEl.innerHTML = citiesHtml;
+      let quantity = document.getElementsByTagName("tr").length;
+      spanEl.innerText = quantity;
     });
   function getResponse(response) {
     return response.json();
