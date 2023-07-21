b6e084d3ff54b30fe142ddbcf8c08f4b8ec2368a
DenisGamzikov <132223000+DenisGamzikov@users.noreply.github.com>
2023-07-21 16:49:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
+const PopulationLimits = {
+  anyCity: {min:0, max:100000000000},
+  littleCity: {min:0, max:100000},
+  averageCity: {min:100000, max:500000},
+  aboveAverageCity: {min:500000, max:1000000},
+  bigCity: {min:1000000, max:10000000000000}
+}
+
 function filter(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
   const filterPopulation = formData.get("filterPopulation");
-  const citySize = formData.get("citySize");
-  console.log(filterPopulation);
-}
+  const cityName = formData.get("cityName");
+  
 
+  
+  const Limits = PopulationLimits[filterPopulation]
+  console.log({Limits })
+ filterByPopulation(Limits.min, Limits.max)
+ const rowElements = document.querySelectorAll("tbody tr");
+  rowElements.forEach((rowEl) => {
+    rowEl.classList.remove("active-tr");
+    rowEl.classList.remove("d-none");
+    city = rowEl.querySelector(".city-name").innerText
+    population = rowEl.querySelector(".population").innerText;
+    if (population >= Limits.min && population <= Limits.max && city.includes(cityName)) {
+      rowEl.classList.add("active-tr");
+    } else {
+      rowEl.classList.add("d-none");
+    }
+    let quantity = document.querySelectorAll("tr.active-tr").length;
+    spanEl.innerText = quantity;
+});
+}
+function filterByPopulation() {
+  //
+}
 const loaderEl = document.getElementById("loader");
 const spanEl = document.getElementById("counter");
 function loadingData() {
@@ -20,7 +49,7 @@ function loadingData() {
       cities.forEach((city) => {
         citiesHtml += `
     <tr>
-    <td> ${city.address}</td>
+    <td class="city-name"> ${city.city}</td>
     <td class="population">${city.population}</td>
     <td class="age">${2023 - city.foundation_year}</td>
     </tr>`;
@@ -35,3 +64,4 @@ function loadingData() {
   }
 }
 loadingData();
+
